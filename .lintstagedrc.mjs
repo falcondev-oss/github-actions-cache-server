1001d89804236ffaafa92787fab9f4dcd9269cd8
Louis Haftmann <30736553+LouisHaftmann@users.noreply.github.com>
2024-02-09 22:42:37 +0100

    chore: fix lint-staged config

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,12 +1,10 @@
-export default () => {
-  const runInPackage = undefined // replace with package name, eg: '@company/package'
-  const pnpmExec = runInPackage ? `pnpm --filter ${runInPackage} exec ` : ''
+const runInPackage = undefined // replace with package name, eg: '@company/package'
+const pnpmExec = runInPackage ? `pnpm --filter ${runInPackage} exec ` : ''
 
-  return {
-    '*.{vue,?([cm])[jt]s?(x),y?(a)ml,json?(c),md,html,?(s)css}': [
-      `${pnpmExec}eslint --fix --cache`,
-      `${pnpmExec}prettier --write --cache`,
-    ],
-    '*.{vue,?([cm])ts?(x)}': () => `${pnpmExec}tsc -p tsconfig.json --noEmit --composite false`, // run once for all files
-  }
+export default {
+  '*.{vue,?([cm])[jt]s?(x),y?(a)ml,json?(c),md,html,?(s)css}': [
+    `${pnpmExec}eslint --fix --cache`,
+    `${pnpmExec}prettier --write --cache`,
+  ],
+  '*.{vue,?([cm])ts?(x)}': () => `${pnpmExec}tsc -p tsconfig.json --noEmit --composite false`, // run once for all files
 }
