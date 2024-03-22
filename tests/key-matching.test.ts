d0df9b5d59b3809d99fbc394484d9c5b44d9767f
Louis Haftmann <30736553+LouisHaftmann@users.noreply.github.com>
2024-03-22 07:48:08 +0100

    test: key matching

--- tests/key-matching.test.ts
+++ tests/key-matching.test.ts
@@ -1,10 +1,86 @@
-import { describe, expect, it } from 'vitest'
+import { beforeEach, describe, expect, test } from 'vitest'
 
-import { cacheApi } from '@/tests/utils'
+import { findKeyMatch, pruneKeys, touchKey } from '@/lib/db'
+import { sleep } from '@/tests/utils'
 
-describe('test', () => {
-  it('should work', async () => {
-    const res = await cacheApi.get('/cache')
-    expect(res.status).toBe(400)
+describe('key matching', () => {
+  beforeEach(() => pruneKeys())
+
+  const version = '0577ec58bee6d5415625'
+  test('exact primary match', async () => {
+    await touchKey('cache-a', version)
+
+    const match = await findKeyMatch({
+      key: 'cache-a',
+      version,
+    })
+    expect(match).toBeDefined()
+    expect(match!.key).toBe('cache-a')
+    expect(match!.version).toBe(version)
+  })
+  test('exact restore key match', async () => {
+    await touchKey('cache-a', version)
+
+    const match = await findKeyMatch({
+      key: 'cache-b',
+      version,
+      restoreKeys: ['cache-a'],
+    })
+    expect(match).toBeDefined()
+    expect(match!.key).toBe('cache-a')
+    expect(match!.version).toBe(version)
+  })
+  test('prefixed restore key match', async () => {
+    await touchKey('prefixed-cache-a', version)
+
+    const match = await findKeyMatch({
+      key: 'prefixed-cache-b',
+      version,
+      restoreKeys: ['prefixed-cache'],
+    })
+    expect(match).toBeDefined()
+    expect(match!.key).toBe('prefixed-cache-a')
+    expect(match!.version).toBe(version)
+  })
+  test('restore key match with multiple keys', async () => {
+    await touchKey('cache-a', version)
+    await touchKey('cache-b', version)
+
+    const match = await findKeyMatch({
+      key: 'cache-c',
+      version,
+      restoreKeys: ['cache-a', 'cache-b'],
+    })
+    expect(match).toBeDefined()
+    expect(match!.key).toBe('cache-a')
+    expect(match!.version).toBe(version)
+  })
+  test('prefixed restore key match with multiple keys returns newest key', async () => {
+    await touchKey('prefixed-cache-a', version)
+    await sleep(10)
+    await touchKey('prefixed-cache-b', version)
+
+    const match = await findKeyMatch({
+      key: 'prefixed-cache-c',
+      version,
+      restoreKeys: ['prefixed-cache'],
+    })
+    expect(match).toBeDefined()
+    expect(match!.key).toBe('prefixed-cache-b')
+    expect(match!.version).toBe(version)
+  })
+  test('restore key prefers exact match over prefixed match', async () => {
+    await touchKey('prefixed-cache', version)
+    await sleep(10)
+    await touchKey('prefixed-cache-a', version)
+
+    const match = await findKeyMatch({
+      key: 'prefixed-cache-b',
+      version,
+      restoreKeys: ['prefixed-cache'],
+    })
+    expect(match).toBeDefined()
+    expect(match!.key).toBe('prefixed-cache')
+    expect(match!.version).toBe(version)
   })
 })
--- tests/utils.ts
+++ tests/utils.ts
@@ -9,3 +9,7 @@ export const cacheApi = ky.extend({
   prefixUrl: 'http://localhost:3000/test_token/_apis/artifactcache',
   throwHttpErrors: false,
 })
+
+export function sleep(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms))
+}
