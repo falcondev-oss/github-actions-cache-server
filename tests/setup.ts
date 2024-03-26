8ba5fcedbe015b3570bbf55e6d91c2df0118778d
Louis Haftmann <30736553+LouisHaftmann@users.noreply.github.com>
2024-03-26 17:55:59 +0100

    test: wait for nitro ready hook on startup

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,30 +1,31 @@
 import { build, createDevServer, createNitro, prepare } from 'nitropack'
-import waitOn from 'wait-on'
 
-import type { Nitro } from 'nitropack'
+import type { Nitro, NitroDevServer } from 'nitropack'
 
 process.env.BASE_URL = 'http://localhost:3000'
 process.env.DATA_DIR = '.data'
 process.env.CACHE_SERVER_TOKEN = 'test_token'
 
 let nitro: Nitro
+let server: Awaited<ReturnType<NitroDevServer['listen']>>
 export async function setup() {
   nitro = await createNitro({
     dev: true,
     preset: 'nitro-dev',
   })
-  const server = createDevServer(nitro)
-  await server.listen(3000, {
+  server = await createDevServer(nitro).listen(3000, {
     hostname: '0.0.0.0',
     autoClose: true,
   })
   await prepare(nitro)
-  await build(nitro)
-  await waitOn({
-    resources: ['http://localhost:3000'],
+  const ready = new Promise<void>((resolve) => {
+    nitro.hooks.hook('dev:reload', () => resolve())
   })
+  await build(nitro)
+  await ready
 }
 
 export async function teardown() {
+  await server?.close()
   await nitro?.close()
 }
