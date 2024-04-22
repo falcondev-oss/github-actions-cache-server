76428fa80547aedee301eb840650ebc22c6f708c
Louis Haftmann <30736553+LouisHaftmann@users.noreply.github.com>
2024-04-22 07:27:54 +0200

    fix: create folder of sqlite file

--- db-drivers/sqlite.ts
+++ db-drivers/sqlite.ts
@@ -1,3 +1,6 @@
+import fs from 'node:fs/promises'
+import path from 'node:path'
+
 import SQLite from 'better-sqlite3'
 import { SqliteDialect } from 'kysely'
 import { z } from 'zod'
@@ -8,7 +11,8 @@ export const sqliteDriver = defineDatabaseDriver({
   envSchema: z.object({
     DB_SQLITE_PATH: z.string(),
   }),
-  setup({ DB_SQLITE_PATH }) {
+  async setup({ DB_SQLITE_PATH }) {
+    await fs.mkdir(path.dirname(DB_SQLITE_PATH), { recursive: true })
     return new SqliteDialect({
       database: new SQLite(DB_SQLITE_PATH),
     })
