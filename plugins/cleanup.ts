dc22943fc9ce2fad5e1360fa97ce967205f66720
Louis Haftmann <30736553+LouisHaftmann@users.noreply.github.com>
2024-10-18 18:57:48 +0200

    build(deps): upgrade deps

--- plugins/cleanup.ts
+++ plugins/cleanup.ts
@@ -9,7 +9,7 @@ import { logger } from '@/lib/logger'
 export default defineNitroPlugin(() => {
   if (ENV.CLEANUP_OLDER_THAN_DAYS === 0) return
 
-  const job = Cron('0 0 * * *') // daily
+  const job = new Cron('0 0 * * *') // daily
   const nextRun = job.nextRun()
   logger.info(
     `Cleaning up cache entries older than ${colorize('blue', `${ENV.CLEANUP_OLDER_THAN_DAYS}d`)} with schedule ${colorize('blue', job.getPattern() ?? '')}${nextRun ? ` (next run: ${nextRun.toLocaleString()})` : ''}`,
