97511d49c5da66d46daba79a0b03419ae69b43a7
Louis Haftmann <30736553+LouisHaftmann@users.noreply.github.com>
2025-03-04 07:48:11 +0100

    fix(proxy): disable passthrough websockets

--- lib/proxy.ts
+++ lib/proxy.ts
@@ -33,6 +33,7 @@ export async function initializeProxy() {
     )
     .thenForwardTo(`http://localhost:${ENV.NITRO_PORT}`)
 
+  await server.forAnyWebSocket().thenPassThrough()
   await server.forUnmatchedRequest().thenPassThrough()
 
   await server.start(port)
