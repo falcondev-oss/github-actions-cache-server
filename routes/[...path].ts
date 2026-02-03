f94322a24ecf80dc15987c66e1f970ece02f6104
Michael Häusler <haeusler@ponton.de>
2026-02-03 23:57:47 +0100

    GHE support (#1)
    * made DEFAULT_ACTIONS_RESULTS_URL configurable

--- routes/[...path].ts
+++ routes/[...path].ts
@@ -1,8 +1,7 @@
+import { env } from '~/lib/env'
 import { logger } from '~/lib/logger'
 
-const DEFAULT_ACTIONS_RESULTS_URL = 'https://results-receiver.actions.githubusercontent.com'
-
 export default defineEventHandler(async (event) => {
-  logger.debug('proxying unknown path', event.path, 'to', DEFAULT_ACTIONS_RESULTS_URL)
-  return proxyRequest(event, `${DEFAULT_ACTIONS_RESULTS_URL}${event.path}`)
+  logger.debug('proxying unknown path', event.path, 'to', env.DEFAULT_ACTIONS_RESULTS_URL)
+  return proxyRequest(event, `${env.DEFAULT_ACTIONS_RESULTS_URL}${event.path}`)
 })
